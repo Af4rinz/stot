a777e082f6d6b2ade09a7c84e28064cd8e57eac6
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2022-02-11 22:44:48 +0330

    now MTOT!

---
+++ src/KalmanFilter.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+class KalmanFilter:
+    def __init__(self, xk, Pk, A, B, Q, H, R):
+        """
+        @brief          Manual Initialization
+        @param xk:      state estimate
+        @param Pk:      covariance matrix
+        @param A:       transition/prediction step matrix (Fk)
+        @param B:       control matrix
+        @param Q:       uncertainty/noise covariance
+        @param H:       sensor matrix
+        @param R:       sensor noise covariance
+        """
+        self.xk = xk
+        self.Pk = Pk
+        self.A = A
+        self.B = B
+        self.Q = Q
+        self.H = H
+        self.R = R
+
+    def __init__(self):
+        dt = 1
+        #position measurement error
+        Bx = 0.001
+        #velocity measurement error
+        Bv = 0.5
+        # state estimate
+        self.xk = np.zeros((4, 1))
+        # covariance matrix
+        self.Pk = np.eye(4)
+        # transition/prediction step matrix
+        self.A = np.array([
+            [1,  0, dt,  0],
+            [0,  1,  0, dt],
+            [0,  0,  1,  0],
+            [0,  0,  0,  1]
+        ])
+        # noise/uncertainty covariance
+        self.Q  = np.eye(4)
+        # sensor model
+        self.H = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0]
+            ])
+        # sensor noise
+        self.R = np.array([
+                [1, 0],
+                [0, 1]
+            ]) 
+        # control matrix
+        self.B = np.array([dt ** 2 / 2, dt ** 2 / 2, dt, dt])
+
+    def predict(self):
+        u = 0
+        # state estimate X
+        self.xk = np.dot(self.A, self.xk) + np.dot(self.B, u)
+        self.Pk = np.dot(np.dot(self.A, self.Pk), self.A.T) + self.Q
+        return self.xk
+
+    def correct(self, zk):
+        """ 
+        @brief  Measurement Update phase
+        @param  zk: measurement
+        @return xk: state estimate
+        """
+        n = self.A.shape[1]
+        # temp = H * Pk * H.T + R
+        temp = np.dot(self.H, np.dot(self.Pk, self.H.T)) + self.R
+        # K' = Pk * H.T * inv(H * Pk * H.T + R)
+        kalman_gain = np.dot(
+                np.dot(self.Pk, self.H.T),
+                np.linalg.inv(temp)
+            )
+        
+        # y = zk - H * xk
+        y = zk - np.dot(self.H, self.xk)
+        # x'k = xk + K * (zk - H * xk)
+        self.xk = self.xk + np.dot(kalman_gain, y)
+
+        # identity matrix
+        I = np.eye(n)
+        # P'k = (-K * H * Pk) * (-K * H)T + K * R * K.T
+        # self.Pk = np.dot(
+        #         np.dot(
+        #             I - np.dot(kalman_gain, self.H),
+        #             self.Pk
+        #         ),
+        #         (I - np.dot(kalman_gain, self.H)).T
+        #     ) \
+        #     + np.dot(
+        #             np.dot(kalman_gain, self.R), 
+        #             kalman_gain.T
+        #         )
+        self.Pk = self.Pk - np.dot(np.dot(kalman_gain, self.H), self.Pk)
+        return self.xk
+    
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 import cv2
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
-def load_video(path, show=False, f=0.5):
+def load_video(path, show=False, f=1):
     cap = cv2.VideoCapture(path)
     ret, frame = cap.read()
     frames = []
@@ -20,10 +22,8 @@ def load_video(path, show=False, f=0.5):
 def click_event(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(x, y)
-        # init_points.append((x, y))
 
 def get_points(img):
-    # global init_points 
     cv2.imshow('image', img)
     cv2.setMouseCallback('image', click_event)
     cv2.waitKey(0)
@@ -31,3 +31,63 @@ def get_points(img):
 
 def save_output(path='./output/', framerate=30, output_name='output.mp4'):
     return os.system(f"ffmpeg -r {framerate} -i {path}%4d.jpg -crf 15 -b 800k -vcodec mpeg4 -y {output_name}")
+
+def clean_frames(frames, background, k=5):
+    """
+    @brief  Apply morphological operations to remove noise and background.
+    @param  frames      list of frames
+    @param  background  averaged background
+    @param  k           kernel size (default 5)
+    @return             list of cleaned frames
+    @return             list of contours sorted by area per frame
+    """
+    approx_objects = []
+    sorted_contours = []
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(k),int(k)))
+    for frame in frames:
+        bg_remove = cv2.absdiff(
+            cv2.cvtColor(cv2.medianBlur(frame,7), cv2.COLOR_BGR2GRAY),
+            cv2.medianBlur(background,7)
+            )
+        _, approx_obj = cv2.threshold(bg_remove, 0, 255, cv2.THRESH_OTSU)
+        approx_obj = cv2.morphologyEx(approx_obj, cv2.MORPH_OPEN, kernel)
+        approx_objects.append(approx_obj)
+
+        contours, hierarchy = cv2.findContours(approx_obj, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cont_area = np.array([cv2.contourArea(c) for c in contours])
+        cont_areas = []
+        for c in contours:
+            cont_areas.append(cv2.contourArea(c))
+        scontours = sorted(zip(cont_areas, contours), key=lambda x: x[0], reverse=True)
+        sorted_contours.append(scontours)
+    return approx_objects, sorted_contours
+
+def get_background(frames):
+    """
+    @brief  Extract the stationary background of a video.
+    @param  frames: list of frames
+    @return averaged background
+    """
+    indices = np.random.randint(0, len(frames), size=30)
+    sample_frames = [frames[i] for i in indices]
+    avg_frames = np.median(sample_frames, axis=0).astype(np.uint8)
+    background = cv2.cvtColor(avg_frames, cv2.COLOR_BGR2GRAY)
+    return background
+
+def get_index_cmap(count, cmap_name='viridis'):
+    """
+    @brief  Get the colours of indexed contours.
+    @param  count:      number of contours
+    @param  cmap_name:  colormap (default 'viridis')
+    @return colour of contour
+    """
+    colours = []
+    cmap = plt.cm.get_cmap(cmap_name)
+    for i in range(count):
+        v = i / (count)
+        r, g, b = [int(x) for x in cmap(v, bytes=True)[:3]]
+        colours.append((b, g, r))
+    return colours
+
+
+
